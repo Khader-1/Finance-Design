1e0aef05a6230bf1266711a01a0f78a7b0dbbfaf
Khader-1 <khader2020kh@gmail.com>
2022-12-24 13:35:58 +0200

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
-import React from "react";
+import { Billing, Business, CardDeal, Clients, CTA, Hero, Navbar, Stats, Testimonials } from "./components";
 import styles from "./styles";
 
 function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
-        <div className={`${styles.boxWidth}`}>content</div>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="HooBank" className="w-[124px] h-[32px]" />
+      <ul className="hidden sm:flex items-center justify-end flex-1">
+        {navLinks.map((link, index) => (
+          <li
+            key={link.id}
+            className={index === navLinks.length - 1 ? `` : `mr-10`}
+          >
+            <a
+              href={`#${link.id}`}
+              className="text-white font-poppins font-normal cursor-pointer text-[16px]"
+            >
+              {link.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div
+        className="sm:hidden flex items-center justify-end cursor-pointer"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <img
+          src={isOpen ? close : menu}
+          alt="menu"
+          className="object-contain h-[28px] w-[28px]"
+        />
+      </div>
+      <div
+        className={`sm:hidden ${
+          isOpen ? `block` : `hidden`
+        } flex flex-col items-center justify-center bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar p-6`}
+      >
+        <ul className="flex items-center justify-end flex-1 flex-col">
+          {navLinks.map((link, index) => (
+            <li
+              key={link.id}
+              className={index === navLinks.length - 1 ? `` : `mb-10`}
+            >
+              <a
+                href={`#${link.id}`}
+                className="text-white font-poppins font-normal cursor-pointer text-[16px]"
+              >
+                {link.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+}
